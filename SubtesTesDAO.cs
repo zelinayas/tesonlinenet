612b626eeb5ed42203f00cc93ff0ecde08b98b8b
zelinayas <zelina.yasinta@gmail.com>
2018-05-02 13:54:41 +0700

    so
    baru yahhhh

--- SubtesTesDAO.cs
+++ SubtesTesDAO.cs
@@ -10,20 +10,20 @@ namespace TesOnlineTes
         SubtesDAO sub = new SubtesDAO(); //nama DAO
         
 
-        //[TestMethod]
-        //public void TestMethod4()
-        //{
+        [TestMethod]
+        public void TestMethodDetail()
+        {
 
-        //    int id = 0;
-        //    //SOAL expectedResult = null; //nama model //bisa pakai .isNull
-        //    SUBTES result = subtes.detail(id);
-        //    Assert.IsNull(result);
+            int id = 0;
+            SOAL expectedResult = null; //nama model //bisa pakai .isNull
+            SUBTES result = sub.detail(id);
+            //Assert.IsNull(result);
+            Assert.AreEqual(expectedResult, result);
 
-
-        //}
+        }
 
         [TestMethod]
-        public void TestMethod5()
+        public void TestMethodTampilDataBenar()
         {
             
             int id = 1;
@@ -31,6 +31,50 @@ namespace TesOnlineTes
             Assert.IsNotNull(result); //jika sudah diisi akan ceklist
 
         }
+
+        //[TestMethod]
+        //public void TestMethodDetail()
+        //{
+
+        //    int ID = 1;
+        //    SUBTES subt = sub.detail(ID);
+        //    Assert.AreEqual("ubah lagi", subt.DURASI_SUB);
+            
+        //}
+
+        [TestMethod]
+        public void TestMethodGetall()
+        {
+
+            Assert.IsNotNull(sub.getAll());
+
+        }
+
+        [TestMethod]
+        public void TestMethodTambahSubtes()
+        {
+
+
+            SUBTES s = new SUBTES();
+            s.ID_SUBTES = 3;
+            s.DURASI_SUB = DateTime.Now;
+            int x = sub.add(s);
+            Assert.AreEqual(1,x);
+
+        }
+
+        //[TestMethod] //berhasil delete 3
+        //public void TestMethodDeleteSubtesBenar()
+        //{
+
+
+        //    SUBTES ss = sub.detail(3);
+        //    Assert.IsNotNull(sub);
+        //    //ss.isPermanent = false;
+        //    int a = sub.delete(3);
+        //    Assert.AreEqual(1, a);
+
+        //}
 
         [TestMethod]
         public void TestMethod6()
@@ -48,17 +92,29 @@ namespace TesOnlineTes
         public void TestMethodEdit()
         {
 
-            SUBTES cekDetail =  sub.detail(1);
+            SUBTES cekDetail =  sub.detail(2);
             Assert.IsNotNull(cekDetail);
-            //SUBTES sub = new SUBTES();
-            //sub.ID_SUBTES = 1;
-            cekDetail.ID_SUBTES = 1;
+            //cekDetail.ID_SUBTES = 2;
             cekDetail.DURASI_SUB = DateTime.Now;
-            int x = sub.edit(1, cekDetail);
+            int x = sub.edit(2, cekDetail);
             Assert.AreEqual(1,x);
-            //jancuk
+            
 
         }
+
+        //[TestMethod]
+        //public void TestMethodDelete()
+        //{
+
+        //    //SUBTES subt = sub.detail(2);
+        //   // subt.ID_SUBTES = 2;
+        //    int x = sub.delete(2);
+        //    Assert.(2);
+
+
+        //}
+
+
 
     }
 
